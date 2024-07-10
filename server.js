8a9e4620ada4bc0f710576df23f070b8045759b7
reemalwahaibi <reemalwahaibi99@outlook.com>
2024-07-10 12:51:19 +0400

    hhg

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.get('/*', function(req,res){
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
- //yes the main is here before rebase
+ //yes the main is here before rebase mm
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
